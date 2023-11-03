2b8c05cb084a423a33b3ed8e2e0084516e654da2
ADESH PAUL <pauladesh@gmail.com>
2023-11-03 09:13:54 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12341234
+let accountEmail = "pauladesh@gmail.com"
+var accountPassword = "123"
+let accountState;
+
+accountEmail = "addxh"
+accountPassword = "2121212121"
+accountCity = "Bengaluru"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("Adesh")
+
+let name = "Adesh" 
+let age = 29 
+let isLoggedIn = false
+let state = null
+let x = undefined
+
+console.table([name, age, isLoggedIn, typeof age, 
+    typeof name, typeof isLoggedIn, typeof state, typeof x])
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+// console.log(typeof age)
